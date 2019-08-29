4290cf5603b7aa363d16c3285750ef3354484bdc
Karol Depka <karol-depka@users.noreply.github.com>
2019-08-29 14:45:24 +0200

    Working prototype of other profiles in TopicFriendsShared for InnoTopicWebsite

---
+++ other-profiles/other-profiles-core/UserOtherProfiles.ts
@@ -0,0 +1,22 @@
+/** TODO: TS mapped type: UserOtherProfilesDescriptorVals<OtherProfile> */
+import { OtherProfile } from './OtherProfile';
+
+export class UserOtherProfiles {
+
+  TopicFriends?: OtherProfile;
+  linkedIn?: OtherProfile;
+  gitHub?: OtherProfile;
+  stackOverflow?: OtherProfile;
+  twitter?: OtherProfile;
+  facebook?: OtherProfile;
+  website?: OtherProfile;
+
+  // Phone number(s)/whatsapp
+  // telegram (phone(s)?)
+  // google / hangouts
+  // slack?
+  // TODO: companyWebsite
+  // blog(s)
+  // email(s)
+
+}
---
+++ other-profiles/user-other-profile-icon/user-other-profile-icon.component.ts
@@ -0,0 +1,24 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { UserOtherProfileDescriptor } from '../other-profiles-core/UserOtherProfilesDescriptors';
+
+@Component({
+  selector: 'app-user-other-profile-icon',
+  templateUrl: './user-other-profile-icon.component.html',
+  styleUrls: ['./user-other-profile-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserOtherProfileIconComponent implements OnInit {
+
+  @Input() readonly descriptor: UserOtherProfileDescriptor
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
